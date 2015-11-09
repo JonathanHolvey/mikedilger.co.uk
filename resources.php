21fef75759ba91acbf2c2f262b53f4f4db523e6a
Jonathan Holvey <jonathan.holvey@outlook.com>
2015-11-09 19:24:24 +1000

    Add viewport meta tag

--- resources.php
+++ resources.php
@@ -1,3 +1,4 @@
+<meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="styles.css" type="text/css" />
 <script type="text/javascript" src="script/jquery.js"></script>
 <script type="text/javascript" src="script/common.js"></script>
