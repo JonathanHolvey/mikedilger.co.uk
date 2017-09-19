7b642ddb849f7932257e292568b47b7bd263885a
Jonathan Holvey <jonathan.holvey@outlook.com>
2017-09-20 06:43:41 +1000

    Force cache refresh for styles.css

--- resources.php
+++ resources.php
@@ -1,5 +1,5 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="icon" href="data:,">
-<link rel="stylesheet" href="styles.css" type="text/css" />
+<link rel="stylesheet" href="styles.css?v=2" type="text/css" />
 <script type="text/javascript" src="script/jquery.js"></script>
 <script type="text/javascript" src="script/common.js"></script>
