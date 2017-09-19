fd6c04c939a87e0db72c4b2f0be413f87153122a
Jonathan Holvey <jonathan.holvey@outlook.com>
2017-09-20 06:28:31 +1000

    Disable favicon.ico requests with link tag

--- resources.php
+++ resources.php
@@ -1,4 +1,5 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="icon" href="data:,">
 <link rel="stylesheet" href="styles.css" type="text/css" />
 <script type="text/javascript" src="script/jquery.js"></script>
 <script type="text/javascript" src="script/common.js"></script>
