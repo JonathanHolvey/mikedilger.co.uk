20176beab2b6d07f84d7bc1598ec066d868ea3e4
Jonathan Holvey <jonathan.holvey@outlook.com>
2017-09-23 14:12:15 +1000

    Tidy Google Analytics tracking code

--- templates/defer.php
+++ templates/defer.php
@@ -4,9 +4,8 @@
 <!-- Global Site Tag (gtag.js) - Google Analytics -->
 <script async src="https://www.googletagmanager.com/gtag/js?id=UA-15642521-1"></script>
 <script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments)};
-  gtag('js', new Date());
-
-  gtag('config', 'UA-15642521-1');
+	window.dataLayer = window.dataLayer || [];
+	function gtag(){dataLayer.push(arguments);}
+	gtag('js', new Date());
+	gtag('config', 'UA-15642521-1');
 </script>
