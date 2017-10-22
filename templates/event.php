b9db82e10456a15f9189a35d8d1cfdad97039b3f
Jonathan Holvey <jonathan.holvey@outlook.com>
2017-10-22 12:46:38 +1000

    Add microdata to event template and tweak styles

--- templates/event.php
+++ templates/event.php
@@ -1,6 +1,13 @@
 <?php include_once(__DIR__ . "/../script/functions.php") ?>
 
-<div class="event" data-id="<?= $event["id"] ?>">
-	<h2><?= escape($event["title"]) ?></h2>
-	<div class="time"><?= date("l j F Y", (int)$event["start"]) ?></div>
+<div class="event" itemscope itemtype="http://schema.org/Event" data-id="<?= $event["id"] ?>">
+	<h2 itemprop="name"><?= escape($event["title"]) ?></h2>
+	<?php if (!empty($event["description"])): ?>
+		<p itemprop="description"><?= escape($event["description"]) ?></p>
+	<?php endif ?>
+	<div class="time" itemprop="startDate" content="<?= date("c", $event["start"]) ?>">
+		<?= date("l j F Y", (int)$event["start"]) ?></div>
+	<?php if (!empty($event["location"])): ?>
+		<div class="location" itemprop="location"><?= $event["location"] ?></div>
+	<?php endif ?>
 </div>
--- templates/tweet.php
+++ templates/tweet.php
@@ -2,5 +2,8 @@
 
 <div class="tweet" data-id="<?= $tweet["id"] ?>">
 	<h2><?= addLinks(escape($tweet["text"])) ?></h2>
-	<a class="time" href="<?= $tweet["url"] ?>" title="View on twitter.com"><?= date("l j F Y", (int)$tweet["time"]) ?> (<?= friendlyAge($tweet["time"]) ?> ago)</a>
+	<a class="time" href="<?= $tweet["url"] ?>" title="View on twitter.com">
+		<span><?= date("l j F Y", (int)$tweet["time"]) ?></span>
+		<span>(<?= friendlyAge($tweet["time"]) ?> ago)</span>
+	</a>
 </div>
