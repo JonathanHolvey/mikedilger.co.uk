507b9d32e7c90dc1572306a159da248466fcf05d
Jonathan Holvey <jonathan.holvey@outlook.com>
2021-02-05 22:06:36 +1000

    Move Twitter and calendar secrets to environment variables

--- web/script/get-updates.php
+++ web/script/get-updates.php
@@ -4,8 +4,13 @@ include "updates.php";
 header("Content-type: application/json");
 $output = array("tweets" => [], "events" => []);
 
-$twitterKeys = new KeyStore("801bd3e4-f9e7-4495-9b20-f083fe2d9960");
-$twitterAuth = new TwitterOAuth(...array_values($twitterKeys->keys));
+$twitterKeys = [
+    getenv("TWITTER_CONSUMER_KEY"),
+    getenv("TWITTER_CONSUMER_SECRET"),
+    getenv("TWITTER_ACCESS_TOKEN"),
+    getenv("TWITTER_ACCESS_SECRET"),
+];
+$twitterAuth = new TwitterOAuth(...$twitterKeys);
 $twitter = new TwitterCache($twitterAuth, "DilgerTV", 20, CACHE_DIR . "/tweets.json");
 $twitter->getTweets();
 // Render tweet HTML from template and add to output array
@@ -16,8 +21,7 @@ foreach ($twitter->newTweets() as $tweet) {
 	array_unshift($output["tweets"], $tweet);
 }
 
-$calendarKeys = new Keystore("e0ef9123-57d5-43ed-ad17-1f86c7a67ba3");
-$calendarUrl = $calendarKeys->keys["url"];
+$calendarUrl = getenv("EVENTS_CALENDAR_URL");
 $calendar = new CalendarCache($calendarUrl, 60, 60, CACHE_DIR . "/events.json");
 $calendar->getEvents();
 // Render event HTML from template and add to output array
--- web/script/updates.php
+++ web/script/updates.php
@@ -150,27 +150,3 @@ class CalendarCache {
 		}
 	}
 }
-
-class KeyStore {
-	function __construct($id) {
-		$this->id = $id;
-		$this->filename = CACHE_DIR . "/keystore-" . $id . ".json";
-		$this->loadKeys();
-	}
-	private function cacheKeys() {
-		if (!is_file($this->filename)) {
-			$url = "https://dev.rocketchilli.com/keystore/" . $this->id;
-			$data = file_get_contents($url);
-			return ($data and file_put_contents($this->filename, $data));
-		}
-		return true;
-	}
-	private function loadKeys() {
-		if ($this->cacheKeys()) {
-			$data = json_decode(file_get_contents($this->filename), true);
-			$this->service = $data["service"];
-			$this->app = $data["app"];
-			$this->keys = array_diff_key($data, array_flip(["id", "service", "app"]));
-		}
-	}
-}
